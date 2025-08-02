b20a515f80f697961a73f728d6be04e0238184fe
hosam <semryhosam@gmail.com>
2025-08-03 02:21:08 +0300

    Registered new apps in settings file

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'questions',
+    'core'
 ]
 
 MIDDLEWARE = [
