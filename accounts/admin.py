93997d8353a3ce3d3e335cabfde5d7c198ca0904
hosam <semryhosam@gmail.com>
2025-08-03 02:42:37 +0300

    Registered UserAccount and UserProfile in admin interface

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import UserAccount, UserProfile
 # Register your models here.
+
+
+@admin.register(UserAccount)
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ['username','first_name', 'last_name']
+
+
+@admin.register(UserProfile)
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ['bio', 'location', 'birth_date']
