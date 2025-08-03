b37f2a320bc640baadfdb7ed94b7990bbe6b05b6
hosam <semryhosam@gmail.com>
2025-08-03 17:47:14 +0300

    Added routes for register, login, logout and profile

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import RegisterView, LoginView, logout_view, UserProfileView
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('u/<str:username>/', UserProfileView.as_view(), name='profile'),
+]
