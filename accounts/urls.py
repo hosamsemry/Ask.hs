bc4a3a685e25f6a337cc8107363b8821057d5958
hosam <semryhosam@gmail.com>
2025-08-04 01:30:15 +0300

    Created routes for follow, follower list, following list and remove follower

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from .views import RegisterView, LoginView, logout_view, UserProfileView, EditProfileView
+from .views import *
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', logout_view, name='logout'),
     path('u/<str:username>/', UserProfileView.as_view(), name='profile'),
-     path('edit-profile/', EditProfileView.as_view(), name='edit_profile'),
+    path('edit-profile/', EditProfileView.as_view(), name='edit_profile'),
+    path('follow/<str:username>/', toggle_follow, name='toggle_follow'),
+    path('<str:username>/followers/', FollowersListView.as_view(), name='followers_list'),
+    path('<str:username>/following/', FollowingListView.as_view(), name='following_list'),
+    path('remove-follower/<str:username>/', remove_follower, name='remove_follower'),
+
 ]
