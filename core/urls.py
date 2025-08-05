ca9c7745ce0d6e35117f660e4e8b8db0822e82db
hosam <semryhosam@gmail.com>
2025-08-05 22:12:58 +0300

    Added a route for users search view

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Home
+from .views import Home, search_users
 
 urlpatterns = [
     path('home/', Home.as_view(), name='home'),
+    path('search-users/', search_users, name='search_users'),
 ]
