9f78b1072bb18ba5dc748323503c2e9760c4eae7
hosam <semryhosam@gmail.com>
2025-08-05 15:21:35 +0300

    Modified home view to display tweets from following users

--- core/views.py
+++ core/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+# qa/views.py
 
-# Create your views here.
-def home_view(request):
-    return render(request, "core/home.html")
+from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from questions.models import Answer
+from accounts.models import UserProfile
+
+class Home(LoginRequiredMixin, ListView):
+    model = Answer
+    template_name = 'core/home.html'
+    context_object_name = 'answers'
+
+    def get_queryset(self):
+        user_profile = UserProfile.objects.get(user=self.request.user)
+        following_users = user_profile.following.all().values_list('user', flat=True)
+        return Answer.objects.filter(responder__in=following_users).order_by('-created_at')
+
