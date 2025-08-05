c2f5d967f074420b6d99d74b9fa2cbe7d48477f7
hosam <semryhosam@gmail.com>
2025-08-05 22:12:36 +0300

    Added a view to enable users search

--- core/views.py
+++ core/views.py
@@ -4,7 +4,11 @@ from django.views.generic import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from questions.models import Answer
 from accounts.models import UserProfile
+from django.http import JsonResponse
+from django.db.models import Q
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 class Home(LoginRequiredMixin, ListView):
     model = Answer
     template_name = 'core/home.html'
@@ -15,3 +19,15 @@ class Home(LoginRequiredMixin, ListView):
         following_users = user_profile.following.all().values_list('user', flat=True)
         return Answer.objects.filter(responder__in=following_users).order_by('-created_at')
 
+
+def search_users(request):
+    query = request.GET.get('q', '').strip()
+    if not query:
+        return JsonResponse({'users': []})
+
+    # First get usernames that start with query, then those that contain query
+    starts_with = User.objects.filter(username__istartswith=query)
+    contains = User.objects.filter(username__icontains=query).exclude(id__in=starts_with.values_list('id', flat=True))
+
+    results = list(starts_with.values('username')) + list(contains.values('username'))
+    return JsonResponse({'users': results})
