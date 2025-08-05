1f1edf3122fcd94e2ff84d748ecd5a77135d2b08
hosam <semryhosam@gmail.com>
2025-08-05 15:24:45 +0300

    Added AnswerLike model and migrated

--- questions/models.py
+++ questions/models.py
@@ -26,3 +26,19 @@ class Answer(models.Model):
 
     def __str__(self):
         return f"Answer to {self.question.to_user.username}"
+    
+
+from django.db import models
+from django.conf import settings
+
+class AnswerLike(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    answer = models.ForeignKey('Answer', on_delete=models.CASCADE, related_name='likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'answer')
+
+    def __str__(self):
+        return f'{self.user.username} liked answer {self.answer.id}'
+
