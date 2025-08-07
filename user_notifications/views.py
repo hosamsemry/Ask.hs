a47aed069948f9a0e06b2cdd7d60bd975d89ec2f
hosam <semryhosam@gmail.com>
2025-08-07 15:01:48 +0300

    Added logic to send notifications for likes

--- user_notifications/views.py
+++ user_notifications/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+from .models import Notification
+from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+def send_like_notification(user, liker):
+    message = f"{liker.username} liked your answer."
+    notification = Notification.objects.create(recipient=user, message=message)
+
+    channel_layer = get_channel_layer()
+    async_to_sync(channel_layer.group_send)(
+        f'notifications_{user.id}',
+        {
+            'type': 'send_notification',
+            'message': message,
+            'notification_id': notification.id,
+        }
+    )
+
