0eadbced1cd49878c8240a6db65e1c961588fead
hosam <semryhosam@gmail.com>
2025-08-07 17:34:39 +0300

    Added sender field in notification model

--- user_notifications/models.py
+++ user_notifications/models.py
@@ -5,6 +5,7 @@ from questions.models import Answer
 User = get_user_model()
 class Notification(models.Model):
     recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
     message = models.TextField()
     is_read = models.BooleanField(default=False)
     timestamp = models.DateTimeField(auto_now_add=True)
@@ -12,4 +13,4 @@ class Notification(models.Model):
 
 
     def __str__(self):
-        return f'Notification for {self.recipient.username}'
+        return f"Notification to {self.recipient.username}: {self.message}"
