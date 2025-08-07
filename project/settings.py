7f3ad34ef6d79dbbe53378366cf8ed760b0dd082
hosam <semryhosam@gmail.com>
2025-08-07 14:54:55 +0300

    Configured channels settings in the settings file

--- project/settings.py
+++ project/settings.py
@@ -39,8 +39,19 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'questions',
-    'core'
+    'core',
+    'channels'
 ]
+ASGI_APPLICATION = 'project.asgi.application'
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
