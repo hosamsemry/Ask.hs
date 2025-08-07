00a046fb21b0ce3f9244d3b4ac1365c99d67be5d
hosam <semryhosam@gmail.com>
2025-08-07 15:04:20 +0300

    Included user_notifications urls in the project main urls file

--- project/urls.py
+++ project/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('core/', include('core.urls')),
     path('qa/', include('questions.urls')),
+    path('notifications/', include('user_notifications.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
