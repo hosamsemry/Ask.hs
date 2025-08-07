5219ca4ad7ba9eafd5c6aaff23f6832ec5638ffa
hosam <semryhosam@gmail.com>
2025-08-07 17:21:30 +0300

    Included answer in send_like_notification and fetch_notifications views

--- user_notifications/views.py
+++ user_notifications/views.py
@@ -5,9 +5,9 @@ from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 
 
-def send_like_notification(user, liker):
+def send_like_notification(user, liker, answer):
     message = f"{liker.username} liked your answer."
-    notification = Notification.objects.create(recipient=user, message=message)
+    notification = Notification.objects.create(recipient=user, message=message,answer=answer,)
 
     channel_layer = get_channel_layer()
     async_to_sync(channel_layer.group_send)(
@@ -24,7 +24,13 @@ def send_like_notification(user, liker):
 def fetch_notifications(request):
     notifications = Notification.objects.filter(recipient=request.user, is_read=False).order_by('-timestamp')
     data = [
-        {"id": n.id, "message": n.message, "timestamp": n.timestamp.strftime("%Y-%m-%d %H:%M")}
+        {
+            "id": n.id,
+            "message": n.message,
+            "timestamp": n.timestamp.strftime("%Y-%m-%d %H:%M"),
+            "answer_id": n.answer.id if n.answer else None,
+            "url": f"/qa/answers/{n.answer.id}/" if n.answer else "#"
+        }
         for n in notifications
     ]
     return JsonResponse({"notifications": data})
