62dddd6ee24ff573656930cd541e7084da2f67b7
hosam <semryhosam@gmail.com>
2025-08-07 14:58:15 +0300

    Created notification model

--- user_notifications/models.py
+++ user_notifications/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+class Notification(models.Model):
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
+    message = models.TextField()
+    is_read = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Notification for {self.recipient.username}'
