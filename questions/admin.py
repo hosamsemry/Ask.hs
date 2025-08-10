636f4c129c61b4643bd028cb45c0609f5ad61374
hosam <semryhosam@gmail.com>
2025-08-10 17:54:24 +0300

    Registered question, answer, answer like in admin panel

--- questions/admin.py
+++ questions/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import Question, Answer, AnswerLike
 # Register your models here.
+
+
+@admin.register(Question)
+class UsersAdmin(admin.ModelAdmin):
+    list_display= [
+        'sender', 'receiver', 'created_at', 'is_answered'
+    ]
+
+
+@admin.register(Answer)
+class UsersAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(AnswerLike)
+class UsersAdmin(admin.ModelAdmin):
+    pass
