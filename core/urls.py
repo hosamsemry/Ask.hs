83b5308e058ecf3630976c9ea42ab19ba2f8278e
hosam <semryhosam@gmail.com>
2025-08-12 02:56:13 +0300

    Moved home route to main project urls file

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import Home, search_users
 
 urlpatterns = [
-    path('home/', Home.as_view(), name='home'),
+    
     path('search-users/', search_users, name='search_users'),
 ]
--- project/urls.py
+++ project/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from core.views import Home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('', Home.as_view(), name='home'),
     path('core/', include('core.urls')),
     path('qa/', include('questions.urls')),
     path('notifications/', include('user_notifications.urls')),
