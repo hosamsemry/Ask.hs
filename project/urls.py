042962cf33a8a17eecb1ab73bae4cd8c99d187db
hosam <semryhosam@gmail.com>
2025-08-14 01:01:50 +0300

    Added view for logging the issue

--- project/urls.py
+++ project/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from core.views import Home
+from debug_view import debug_s3_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,4 +12,5 @@ urlpatterns = [
     path('core/', include('core.urls')),
     path('qa/', include('questions.urls')),
     path('notifications/', include('user_notifications.urls')),
+    path('debug-s3/', debug_s3_view, name='debug_s3'),  # Temporary debug view
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
